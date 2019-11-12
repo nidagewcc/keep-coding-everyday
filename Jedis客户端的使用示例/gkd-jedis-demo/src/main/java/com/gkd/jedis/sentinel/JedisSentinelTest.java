524ac6315c39d8f4b910835b145ccc6fdbd0908b
nidagewcc <nidagewcc@163.com>
2019-11-12 15:40:58 +0800

    jedis sentinel

--- Jedis客户端的使用示例/gkd-jedis-demo/src/main/java/com/gkd/jedis/sentinel/JedisSentinelTest.java
+++ Jedis客户端的使用示例/gkd-jedis-demo/src/main/java/com/gkd/jedis/sentinel/JedisSentinelTest.java
@@ -1,4 +1,4 @@
-package com.gkd.jedis;
+package com.gkd.jedis.sentinel;
 
 
 import redis.clients.jedis.JedisSentinelPool;
@@ -6,23 +6,19 @@ import redis.clients.jedis.JedisSentinelPool;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 哨兵模式-一主二从模式
+ * 环境搭建参考笔记
+ */
 public class JedisSentinelTest {
-
-
     public static void main(String[] args) {
         String masterName = "redis-master";
         Set<String> sentinels = new HashSet<>();
-
         sentinels.add("192.168.52.200:26379");
         sentinels.add("192.168.52.201:26379");
         sentinels.add("192.168.52.202:26379");
-
         JedisSentinelPool pool = new JedisSentinelPool(masterName, sentinels);
-
         pool.getResource().set("kaixin", "happy");
         System.out.println(pool.getResource().get("kaixin"));
-
-        // 测试成功，Happy
-
     }
 }
--- Jedis客户端的使用示例/gkd-jedis-demo/src/main/java/com/gkd/jedis/single/SingleTest.java
+++ Jedis客户端的使用示例/gkd-jedis-demo/src/main/java/com/gkd/jedis/single/SingleTest.java
@@ -1,9 +1,11 @@
-package com.gkd.jedis;
+package com.gkd.jedis.single;
 
 import redis.clients.jedis.Jedis;
 
-public class BaseTest {
-
+/**
+ * 单机版连接测试
+ */
+public class SingleTest {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("192.168.52.128", 6379);
         jedis.set("test", "1");
