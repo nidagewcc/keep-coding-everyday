660e22f63202198d84812d524835d8cde3be6939
nidagewcc <nidagewcc@163.com>
2020-01-20 11:55:34 +0800

    zz

--- 算法相关收集/快速排序/gkd-quick-sort/src/com/gkd/Main.java
+++ 算法相关收集/快速排序/gkd-quick-sort/src/com/gkd/Main.java
@@ -7,32 +7,60 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {21, 32, 43, 98, 54, 45, 23, 4, 66, 86};
 
-        quickSort(arr, 0, 9);
+        quickSort1(arr, 0, 9);
 
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void quickSort(int[] arr, int left, int right) {
+    private static void quickSort(int[] s, int l, int r) {
 
-        // 基准数，取左边第一个元素为基准数
-        int temp = arr[0];
-        int i = left, j = right;
+        if (l < r) {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j) {
+                while (i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if (i < j)
+                    s[i++] = s[j];
 
+                while (i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if (i < j)
+                    s[j--] = s[i];
+            }
+            s[i] = x;
+
+            quickSort(s, l, i - 1); // 递归调用
+            quickSort(s, i + 1, r);
+        }
+    }
+
+    // 。。。 。。。
+    private static void quickSort1(int[] arr, int l, int r) {
+
+        int i = l, j = r, x = arr[l];
         while (i < j) {
-            if (temp >= arr[j]) {
-                arr[i] = arr[j];
-                arr[j] = temp;
 
+            while (arr[j] > x) {
                 j--;
             }
-        }
 
-        while (i < j) {
-            if (arr[i] > temp) {
-                arr[j] = arr[i];
-                arr[i] = temp;
+            arr[j] = x;
+            i++;
+
+            while (arr[i] <= x) {
                 i++;
             }
+
+            arr[i] = x;
+            j--;
+
+            if (i == j) {
+                arr[i] = x;
+            }
+
+            quickSort1(arr, l, i - 1);
+            quickSort1(arr, i + 1, r);
         }
 
     }
